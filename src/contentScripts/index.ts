5a7f1cb8febdecacd03c421ba24360741cb92fb1
Igor Belyaletdinov <igor.belyaletdinov@studio-moderna.com>
2023-08-28 12:43:10 +0200

    Names.

--- src/contentScripts/index.ts
+++ src/contentScripts/index.ts
@@ -6,11 +6,11 @@ import { onMessage } from 'webext-bridge/content-script'
 
 // Firefox `browser.tabs.executeScript()` requires scripts return a primitive value
 (() => {
-  console.info('[vitesse-webext] Hello world from content script')
+  // console.info('[sia-data-wallet-webext] Hello world from content script')
 
   // communication example: send previous tab title from background page
   onMessage('tab-prev', ({ data }) => {
-    console.log(`[vitesse-webext] Navigate from page "${data.title}"`)
+    // console.log(`[sia-data-wallet-webext] Navigate from page "${data.title}"`)
   })
 
   // mount component to context window
