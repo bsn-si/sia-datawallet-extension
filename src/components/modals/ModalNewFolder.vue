21f8f641ab28ed7a9af263addbdc784174cc9659
Igor Belyaletdinov <igor.belyaletdinov@studio-moderna.com>
2023-10-19 10:21:18 +0200

    Applying design

--- src/components/modals/ModalNewFolder.vue
+++ src/components/modals/ModalNewFolder.vue
@@ -1,28 +1,57 @@
 <template>
   <v-f-modal-layout>
-    <div class="sm:flex sm:items-start">
-      <div class="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-blue-50 dark:bg-gray-500 sm:mx-0 sm:h-10 sm:w-10">
-         <svg xmlns="http://www.w3.org/2000/svg"
-               class="h-6 w-6 stroke-blue-600 dark:stroke-blue-100" fill="none" viewBox="0 0 24 24" stroke="none" stroke-width="1.5">
-              <path stroke-linecap="round" stroke-linejoin="round" d="M9 13h6m-3-3v6m-9 1V7a2 2 0 012-2h6l2 2h6a2 2 0 012 2v8a2 2 0 01-2 2H5a2 2 0 01-2-2z" />
-          </svg>
-      </div>
-      <div class="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left w-full">
-        <h3 class="text-lg leading-6 font-medium text-gray-900 dark:text-gray-400" id="modal-title">{{ t('New Folder') }}</h3>
-        <div class="mt-2">
-          <p class="text-sm text-gray-500">{{ t('Create a new folder') }}</p>
-          <input v-model="name" @keyup.enter="createFolder"
-                 class="px-2 py-1 border rounded dark:bg-gray-700/25 dark:focus:ring-gray-600 dark:focus:border-gray-600 dark:text-gray-100 w-full" :placeholder="t('Folder Name')" type="text">
-          <message v-if="message.length" @hidden="message=''" error>{{ message }}</message>
+    <div>
+      <div class="w-full h-[72px] px-4 py-[5px] bg-zinc-800 rounded-tl-[28px] rounded-tr-[28px] justify-start items-center gap-1 inline-flex" style="border-bottom: 1px #938F99 solid">
+        <div class="grow shrink basis-0 flex-col justify-center items-start inline-flex">
+          <div class="h-6 py-2.5 justify-start items-center gap-px inline-flex">
+            <div class="text-zinc-200 text-base font-normal font-['Roboto'] leading-normal tracking-wide">Create folder</div>
+          </div>
+        </div>
+        <div @click="emitter.emit('vf-modal-close')" class="w-12 h-12 flex-col justify-center items-center gap-2.5 inline-flex cursor-pointer">
+          <div class="rounded-[100px] justify-center items-center gap-2.5 inline-flex">
+            <div class="p-2 justify-center items-center gap-2.5 flex">
+              <div class="w-6 h-6 relative">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                  <path d="M19 6.41L17.59 5L12 10.59L6.41 5L5 6.41L10.59 12L5 17.59L6.41 19L12 13.41L17.59 19L19 17.59L13.41 12L19 6.41Z" fill="#CAC4D0"/>
+                </svg>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
+
+
+      <div class="my-6 w-full flex justify-center">
+        <div class="mt-2">
+          <div class="text-gray-500 mb-1 py-2 w-[326px]">
+            <input v-model="name" @keyup.enter="createFolder"
+                   class="px-2 py-1 border rounded dark:bg-gray-700/25 dark:focus:ring-gray-600 dark:focus:border-gray-600 dark:text-gray-100 w-full" :placeholder="t('Folder Name')" type="text">
+
+            <message v-if="message.length" @hidden="message=''" error>{{ message }}</message>
+          </div>
+        </div>
+
+      </div>
+
     </div>
 
     <template v-slot:buttons>
-      <button type="button" @click="createFolder" class="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-blue-600 dark:bg-gray-700 dark:hover:bg-gray-600/75 text-base font-medium text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm">
-        {{ t('Create') }}</button>
-      <button type="button" @click="emitter.emit('vf-modal-close')" class="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm">
-        {{ t('Cancel') }}</button>
+      <div class="py-3">
+        <div @click="emitter.emit('vf-modal-close')" class="w-[143px] h-10 bg-button1-100 rounded-[100px] flex-col justify-center items-center inline-flex mr-4 cursor-pointer" >
+          <div class="self-stretch pl-3 pr-4 py-2.5  bg-opacity-10 justify-center items-center gap-2 inline-flex">
+            <div class="text-center text-purple-300 text-sm font-medium font-['Roboto'] leading-tight tracking-tight">{{ t('Cancel') }}</div>
+          </div>
+        </div>
+
+        <div @click="createFolder" class="w-[143px] h-10 bg-purple-300 rounded-[100px] flex-col justify-center items-center inline-flex cursor-pointer">
+          <div class="pl-5 pr-7 py-2.5 justify-start items-center gap-2 inline-flex">
+            <div class="text-center text-zinc-900 text-sm font-medium font-['Roboto'] leading-tight tracking-tight">
+              <span>{{ t('Create') }}</span>
+            </div>
+          </div>
+        </div>
+
+      </div>
     </template>
   </v-f-modal-layout>
 </template>
