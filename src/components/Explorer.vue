4dda6a83ed1aea291c306d253b272a0516516315
Igor Belyaletdinov <igor.belyaletdinov@studio-moderna.com>
2023-11-08 22:10:19 +0100

    Applying design to explorer.

--- src/components/Explorer.vue
+++ src/components/Explorer.vue
@@ -101,26 +101,32 @@
            @contextmenu.prevent="emitter.emit('vf-contextmenu-show', {event: $event, area: selectorArea, items: getSelectedItems(), target: item })"
            @dragstart="handleDragStart($event,item)"
            @dragover="handleDragOver($event,item)"
+           @mouseenter="handleMouseEnter($event,item)"
+           @mouseleave="handleMouseLeave($event,item)"
            @drop="handleDropZone($event,item)"
            :class="'vf-item-' + randId"
-           class="border border-transparent hover:bg-neutral-50 m-1 dark:hover:bg-gray-700 inline-flex w-[5.5rem] h-20 md:w-24 text-center justify-center select-none"
+           class="border border-transparent m-1 inline-flex w-[5.5rem] h-20 md:w-24 text-center justify-center select-none"
            v-for="(item, index) in getItems(false)" :data-type="item.type" :data-item="JSON.stringify(item)" :data-index="index">
           <div>
-            <div class="relative">
-              <svg v-if="item.type == 'dir'" xmlns="http://www.w3.org/2000/svg" class="h-10 w-10 md:h-12 md:w-12 m-auto fill-sky-500 stroke-sky-500 dark:fill-slate-500 dark:stroke-slate-500" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="1">
+            <div class="relative mb-2">
+              <svg v-if="item.type === 'dir'" xmlns="http://www.w3.org/2000/svg" class="h-10 w-10 md:h-12 md:w-12 m-auto fill-sky-500 stroke-sky-500 dark:fill-slate-500 dark:stroke-slate-500" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="1">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z" />
               </svg>
               <img class="lazy h-10 md:h-12 m-auto" v-else-if="(item.mime_type ?? '').startsWith('image')" :data-src="getImageUrl(adapter.value, item.path)"  :alt="item.basename">
-              <svg v-else xmlns="http://www.w3.org/2000/svg" class="h-10 w-10 md:h-12 md:w-12 m-auto text-neutral-500" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="1">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M7 21h10a2 2 0 002-2V9.414a1 1 0 00-.293-.707l-5.414-5.414A1 1 0 0012.586 3H7a2 2 0 00-2 2v14a2 2 0 002 2z" />
+              <svg v-if="item.type === 'file' && (item.hover || handleSelectedIcon(item))" xmlns="http://www.w3.org/2000/svg" class="h-10 w-10 md:h-12 md:w-12 m-auto text-neutral-500" viewBox="0 0 42 42" fill="none">
+                <path d="M29.2002 1.43415L30.2002 0.409756L29.8002 0H29.2002V1.43415ZM37.4002 9.83415H38.8002V9.21951L38.4002 8.80976L37.4002 9.83415ZM34.7002 39.2341H7.3002V42H34.6002V39.2341H34.7002ZM6.0002 37.8V4.2H3.2002V37.8H6.0002ZM7.3002 2.76585H29.2002V0H7.3002V2.76585ZM36.0002 9.83415V37.8H38.7002V9.83415H36.0002ZM28.2002 2.3561L36.4002 10.7561L38.3002 8.80976L30.1002 0.409756L28.2002 2.3561ZM7.3002 39.2341C6.9002 39.2341 6.6002 39.1317 6.3002 38.8244C6.0002 38.5171 5.9002 38.2098 5.9002 37.8H3.2002C3.2002 38.9268 3.6002 39.9512 4.4002 40.7707C5.2002 41.5902 6.2002 42 7.3002 42V39.2341ZM34.7002 42C35.8002 42 36.8002 41.5902 37.6002 40.7707C38.4002 39.9512 38.8002 38.9268 38.8002 37.8H36.0002C36.0002 38.2098 35.9002 38.5171 35.6002 38.8244C35.3002 39.1317 35.0002 39.2341 34.6002 39.2341V42H34.7002ZM6.0002 4.2C6.0002 3.79024 6.1002 3.48293 6.4002 3.17561C6.7002 2.86829 7.0002 2.76585 7.4002 2.76585V0C6.2002 0 5.2002 0.409756 4.4002 1.22927C3.7002 2.04878 3.2002 3.07317 3.2002 4.2H6.0002Z" fill="#BEA9EE"/>
               </svg>
-              <svg v-if="item.status==='uploading'" aria-hidden="true" class="vf-item-spinner w-6 h-6 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-green-500" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <svg v-else-if="item.type === 'file'" xmlns="http://www.w3.org/2000/svg" class="h-10 w-10 md:h-12 md:w-12 m-auto text-neutral-500" fill="none" viewBox="0 0 42 42" >
+                <path stroke-linecap="round" stroke-linejoin="round" d="M29.2002 1.43415L30.2002 0.409756L29.8002 0H29.2002V1.43415ZM37.4002 9.83415H38.8002V9.21951L38.4002 8.80976L37.4002 9.83415ZM34.7002 39.2341H7.3002V42H34.6002V39.2341H34.7002ZM6.0002 37.8V4.2H3.2002V37.8H6.0002ZM7.3002 2.76585H29.2002V0H7.3002V2.76585ZM36.0002 9.83415V37.8H38.7002V9.83415H36.0002ZM28.2002 2.3561L36.4002 10.7561L38.3002 8.80976L30.1002 0.409756L28.2002 2.3561ZM7.3002 39.2341C6.9002 39.2341 6.6002 39.1317 6.3002 38.8244C6.0002 38.5171 5.9002 38.2098 5.9002 37.8H3.2002C3.2002 38.9268 3.6002 39.9512 4.4002 40.7707C5.2002 41.5902 6.2002 42 7.3002 42V39.2341ZM34.7002 42C35.8002 42 36.8002 41.5902 37.6002 40.7707C38.4002 39.9512 38.8002 38.9268 38.8002 37.8H36.0002C36.0002 38.2098 35.9002 38.5171 35.6002 38.8244C35.3002 39.1317 35.0002 39.2341 34.6002 39.2341V42H34.7002ZM6.0002 4.2C6.0002 3.79024 6.1002 3.48293 6.4002 3.17561C6.7002 2.86829 7.0002 2.76585 7.4002 2.76585V0C6.2002 0 5.2002 0.409756 4.4002 1.22927C3.7002 2.04878 3.2002 3.07317 3.2002 4.2H6.0002Z" fill="#4F378B"/>
+              </svg>
+              <svg v-if="false" aria-hidden="true" class="vf-item-spinner w-6 h-6 mr-2 text-gray-200 animate-spin dark:text-zinc-900 fill-spinner-500" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
                 <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
               </svg>
+              <div v-if="item.status==='uploading'" class="vf-item-spinner-small w-4 h-4 rounded-full animate-spin border-2 border-solid border-spinner-500 border-t-transparent"></div>
               <div class="absolute hidden md:block top-1/2 w-full text-center text-neutral-500" v-if="!(item.mime_type ?? '').startsWith('image') && item.type != 'dir'">{{ ext(item.extension) }}</div>
             </div>
-            <span class="break-all">{{ title_shorten(item.basename) }}</span>
+            <span class="break-all vf-item-caption" :style="{color: item.type === 'file' && item.hover ? '#BEA9EE' : '#938F99'}">{{ title_shorten(item.basename) }}</span>
           </div>
       </div>
 
@@ -288,7 +294,7 @@ const sortBy = (column) => {
   }
 };
 
-const getSelectedItems = () => ds.value.getSelection().map((el) => JSON.parse(el.dataset.item))
+const getSelectedItems = () => ds.value ? ds.value.getSelection().map((el) => JSON.parse(el.dataset.item)) : [];
 
 const handleDragStart = (e, item) => {
   if (e.altKey || e.ctrlKey || e.metaKey) {
@@ -329,6 +335,31 @@ const handleDragOver = (e, item) => {
   }
 };
 
+const handleMouseEnter = (e, item) => {
+  if (e.altKey || e.ctrlKey || e.metaKey) {
+    e.preventDefault();
+    return false;
+  }
+
+  item.hover = true;
+
+};
+
+const handleSelectedIcon = (item) => {
+  if (getSelectedItems().find(selectedItem => selectedItem.path === item.path)) {
+    item.hover = true;
+  }
+};
+
+const handleMouseLeave = (e, item) => {
+  if (e.altKey || e.ctrlKey || e.metaKey) {
+    e.preventDefault();
+    return false;
+  }
+
+  item.hover = false;
+};
+
 const setDragSelect = () => {
   ds.value = new DragSelect({
     area: selectorArea.value,
@@ -367,6 +398,14 @@ const setDragSelect = () => {
   ds.value.subscribe("callback", (	{ items, event, isDragging}) => {
     emitter.emit('vf-nodes-selected', getSelectedItems());
     selectedCount.value = ds.value.getSelection().length;
+
+    getItems().map((item) => {
+      if (getSelectedItems().find(selectedItem => selectedItem.path === item.path)) {
+        item.hover = true;
+      } else {
+        item.hover = false
+      }
+    });
   })
 };
 
@@ -389,5 +428,16 @@ onMounted(() => {
   position: absolute;
   left: calc(50% - 11px);
   top: calc(50% - 9px);
+}
+.vf-item-spinner-small {
+  position: absolute;
+  left: calc(50% - 7px);
+  top: calc(50% - 7px);
+}
+.vf-item-caption {
+  font-size: 13px;
+  line-height: 18px;
+  font-family: Roboto;
+  font-weight: 400;
 }
 </style>
